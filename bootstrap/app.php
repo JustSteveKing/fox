9407795d1ad71b54b8c0127b8e1efda48eb39d68
JustSteveKing <juststeveking@gmail.com>
2017-08-07 08:40:10 +0100

    Remove application middleware for now

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,8 @@ try {
     throw new InvalidPathException($e->getMessage());
 }
 
+session_start();
+
 $app = new Slim\App([
     'settings' => include __DIR__ . '/../config/app.php'
 ]);
@@ -17,14 +19,14 @@ $app = new Slim\App([
 require __DIR__ . '/container.php';
 
 
-// Middleware loading
-$middleware = new hanneskod\classtools\Iterator\ClassIterator(
+// Middleware loading - uncomment if you want all middleware to be loaded for the application, otherwise keep middleware loading for Routes only
+/*$middleware = new hanneskod\classtools\Iterator\ClassIterator(
 	(new Symfony\Component\Finder\Finder)->in(__DIR__ . '/../app/Http/Middleware')
 );
 
 foreach ($middleware->getClassMap() as $classname => $fileInfo) {
 	$app->add(new $classname());
-}
+}*/
 
 require __DIR__ . '/../routes/web.php';
 
