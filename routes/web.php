9673d44ba0f6fbf24404e47968b5fb2e0847bdaa
JustSteveKing <juststeveking@gmail.com>
2017-08-07 08:39:59 +0100

    Adding example middleware to route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
-$app->get('/', 'App\Http\Controllers\HomeController:index')->setName('home-route');
+$app->get('/', 'App\Http\Controllers\HomeController:index')
+    ->setName('home-route')
+    ->add(new App\Http\Middleware\ExampleMiddleware($app));
 
 /*$app->get('/[{name}]', function ($request, $response, $args) {
     // Sample log message
