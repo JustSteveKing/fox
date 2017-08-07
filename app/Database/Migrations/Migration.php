3b88a9e9c10801829b9c671fa664e5ca4c3b06fe
JustSteveKing <juststeveking@gmail.com>
2017-08-07 20:12:40 +0100

    Create base migration class

---
+++ app/Database/Migrations/Migration.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use Phinx\Migration\AbstractMigration;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class Migration extends AbstractMigration
+{
+    protected $schema;
+
+    public function init()
+    {
+        $this->schema = (new Capsule)->schema();
+    }
+}
