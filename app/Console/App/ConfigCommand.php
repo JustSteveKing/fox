4c6fc591df907f1a52d15d39f6ddaa73c0bdbb46
JustSteveKing <juststeveking@gmail.com>
2017-08-09 16:39:24 +0100

    Command output all of your configuration details

--- app/Console/App/ConfigCommand.php
+++ app/Console/App/ConfigCommand.php
@@ -3,6 +3,7 @@
 namespace App\Console\App;
 
 use Noodlehaus\Config;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -20,7 +21,28 @@ class ConfigCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('<info>Application config:</info>');
-        $output->writeln('<info>App Name: ' . $this->config->get('app.name') . '</info>');
+        $config = $this->config->all();
+
+        foreach($config as $key => $item) {
+            $output->writeln('<info>' . ucfirst($key) . '</info>');
+
+            $table = new Table($output);
+            $rows = [];
+
+            foreach($item as $key => $value) {
+                if (is_array($value)) {
+                    array_push($rows, implode(', ', $value));
+                } else {
+                    array_push($rows, $value);
+                }
+            }
+            $table
+                ->setHeaders(array_map('ucwords', array_keys($item)))
+                ->setRows(array(
+                    $rows
+                ));
+            $table->render();
+            $output->writeln('');
+        }
     }
 }
